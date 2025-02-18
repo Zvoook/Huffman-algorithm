5cdc59425de540a3be35b7941452877b78bdf045
Dmitry Danilov <rezviipersik@gmail.com>
2025-02-18 17:27:51 +0300

    Update Compressing.h

--- Compressing.h
+++ Compressing.h
@@ -2,7 +2,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdlib.h>
-#define CODE_SIZE 8
+#define BYTE 8
 #define SIMB 256
 typedef struct NODE {
 	unsigned char symb;
@@ -23,7 +23,6 @@ NODE* create_node(int id, int val);
 LIST* init_list();
 void add_node(LIST* list, int id, int val);
 void print_list(LIST* list);
-void megaswap(int& a, int& b);
 void fill_list(LIST* list, int* arr);
 NODE* Add2LIST(NODE* head, NODE* node);
 NODE* MakeNodeFromNode(NODE* left, NODE* right);
