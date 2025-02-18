0099fbda31efb54ed11fd9d5563aeded79508719
Dmitry Danilov <rezviipersik@gmail.com>
2025-02-18 17:30:46 +0300

    Update Compressing.cpp

--- Compressing.cpp
+++ Compressing.cpp
@@ -6,17 +6,15 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int filesize(FILE* name) {
-	fseek(name, 0L, SEEK_END);
-	long len = ftell(name);
-	fseek(name, 0, SEEK_SET);
+int File_Size(FILE* f) {
+	fseek(f, 0L, SEEK_END);
+	long len = ftell(f);
+	fseek(f, 0, SEEK_SET);
 	return len;
 }
 
 NODE* create_node(int id, int val)
 {
-	/*NODE* node;
-	node = new NODE;*/
 	NODE* node = (NODE*)calloc(1, sizeof(NODE));
 	node->symb = (unsigned char)id;
 	node->isSymb = 1;
@@ -26,17 +24,16 @@ NODE* create_node(int id, int val)
 	node->right = NULL;
 	return node;
 }
+
 LIST* init_list()
 {
-	/*LIST* list;
-	list = new LIST;*/
 	LIST* list = (LIST*)calloc(1, sizeof(LIST));
 	list->head = NULL;
 	list->tail = NULL;
-	//list->head->next = list->tail;
 	list->size = 0;
 	return list;
 }
+
 NODE* Add2LIST(NODE* head, NODE* node) {
 	if (!head) return node;
 	if (node->freq < head->freq) {
@@ -50,34 +47,7 @@ NODE* Add2LIST(NODE* head, NODE* node) {
 	return head;
 }
 
-//void add_node(LIST* list, int id, int val)
-//{
-//	NODE* node = create_node(id, val);
-//	node->next = list->head->next;
-//	/*if (list->size == 0) list->head = node;
-//	if (list->size == 1) list->tail = node;*/
-//	list->head->next = node;
-//	list->size++;
-
-void megaswap(int& a, int& b) {
-	int temp = a;
-	a = b;
-	b = temp;
-}
 void fill_list(LIST* list, int* arr) {
-	/*int max = arr[0], id = 0, size = code_size;
-	for (int a = 0; a < code_size; ++a) {
-		for (int i = 0; i < size; ++i) {
-			if (arr[i] > max) {
-				max = arr[i];
-				id = i;
-			}
-		}
-		add_node(list, id, max);
-		megaswap(arr[id], arr[size - 1]);
-		--size;
-		max = arr[0];
-	}*/
 	for (int i = 0; i < SIZE; i++) {
 		if (arr[i] > 0) {
 			NODE* node = create_node(i, arr[i]);
@@ -85,8 +55,9 @@ void fill_list(LIST* list, int* arr) {
 			list->size++;
 		}
 	}
-	printf("Filling list completed!\n");
+	//cout << "Filling list completed!" << endl;
 }
+
 void print_list(LIST* list)
 {
 	NODE* node = list->head;
@@ -108,6 +79,7 @@ NODE* MakeNodeFromNode(NODE* left, NODE* right)
 	node->next = NULL;
 	return node;
 }
+
 NODE* MakeTreeFromList(NODE* head)
 {
 	while (head && head->next) {
@@ -119,23 +91,23 @@ NODE* MakeTreeFromList(NODE* head)
 		NODE* node = MakeNodeFromNode(left, right);
 		head = Add2LIST(head, node);
 	}
-	printf("Creating tree completed!\n");
+	//cout << "Creating tree completed!" << endl;
 	return head;
 }
+
 void coding(NODE* node, int len, char* code, char* codes[]) {
-  if (!node) return;
-  if (node->left == NULL && node->right==NULL) {
-    code[len]='\0';
-    strcpy(codes[node->symb], code);
-    return;
-  }
-  if (node->left) {
-    code[len] = '0';
-    coding(node->left, len+1, code, codes);
-  }
-  if (node->right) {
-    code[len] = '1';
-    coding(node->right, len+1, code, codes);
-  }
-  //cout << "Element " << node->symb << " coded as the: " << node->code << endl;
+	if (!node) return;
+	if (node->isSymb==1) {
+		code[len]='\0';
+		strcpy(codes[node->symb], code);
+		return;
+	}
+	if (node->left) {
+		code[len] = '0';
+		coding(node->left, len+1, code, codes);
+	}
+	if (node->right) {
+		code[len] = '1';
+		coding(node->right, len+1, code, codes);
+	}
 }
