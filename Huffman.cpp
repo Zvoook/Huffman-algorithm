92905c8c674273d27ce4c1ed780db88e0a2812b8
Dmitry Danilov <rezviipersik@gmail.com>
2025-02-18 17:32:17 +0300

    Update Huffman.cpp

--- Huffman.cpp
+++ Huffman.cpp
@@ -2,29 +2,36 @@
 #include <iostream>
 #include <stdlib.h>
 #include "Compressing.h"
-#define Haffman
-#ifdef Haffman
+#ifdef Huffman
 using std::cin;
 using std::cout;
 using std::endl;
 int main() {
-	FILE* file = fopen("WAR_AND_PEACE.txt", "rb");
-	if (!file) return -1;
-	long length = filesize(file);
-	/*int* amount;
-	amount = new int[code_size];
-	for (int i = 0; i < code_size; ++i) amount[i]=0;*/
+	//Frequency counting
+	FILE* file = fopen("Video.mov", "rb");
+	if (!file) {
+        cout << "Can't open source file!" << endl;
+        return -1;
+    	}
+	long lenght = File_Size(file);
 	int* amount = (int*)calloc(SIZE, sizeof(int));
-	for (int i = 0; i < length; ++i) amount[(unsigned char)fgetc(file)]++;
+	for (int i = 0; i < lenght; ++i) amount[(unsigned char)fgetc(file)]++;
 	fclose(file);
 
+	//Creating List
 	LIST* list = init_list();
 	fill_list(list, amount);
-	//print_list(list);
-	/*char** codes;
-	char* code;
-	codes = new char*[SIZE];
-	code = new char[SIZE];*/
-	list->head=MakeTreeFromList(list->head);
-}
-#endif
+	print_list(list);
+
+	//Creating Tree from List
+	NODE* root=MakeTreeFromList(list->head);
+
+	//Simbols' codes writing
+	char* code = (char*)calloc(SIZE, sizeof(char));
+	char** codes = (char**)calloc(SIZE, sizeof(char*));
+	for (int i = 0; i < SIZE; ++i) codes[i] = (char*)calloc(SIZE, sizeof(char));
+	coding(root, 0, code, codes);
+	for (int i = 0; i < SIZE; i++) {
+		if (amount[i] > 0)
+			printf("'%c' : %s\n", i, codes[i]);
+	}
